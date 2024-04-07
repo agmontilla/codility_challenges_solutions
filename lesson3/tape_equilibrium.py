075482f305bc770820e54f397c0ac8e0bdfde8e0
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-04-06 22:53:29 -0300

    test: add test to check tape equilibrium challenge

--- lesson3/tape_equilibrium.py
+++ lesson3/tape_equilibrium.py
@@ -20,31 +20,19 @@ But finally that solution is very slow (sometimes it reached 6 seconds, above up
 """
 
 
-from typing import List
+async def solution(numbers: list[int]) -> int:
+    """Returns the minimal difference between the sum of the first part and the sum of the second part of the list"""
+    pivot = numbers[0]
 
-
-def solution(A: List) -> int:
-    pivot = A[0]
-
-    left, right = 0, sum(A)
+    left, right = 0, sum(numbers)
     result = []
 
-    if len(A) >= 1:
-        for idx in range(len(A) - 1):
-            left += A[idx]
-            right -= A[idx]
+    if len(numbers) >= 1:
+        for idx in range(len(numbers) - 1):
+            left += numbers[idx]
+            right -= numbers[idx]
             result.append(abs(left - right))
 
         pivot = min(result)
 
     return pivot
-
-
-def main():
-    # sample = [-1000, 1000]
-    sample = [3, 1, 2, 4, 3]
-    print(solution(sample))
-
-
-if __name__ == "__main__":
-    main()
---
+++ tests/lesson3/test_tape_equilibrium.py
@@ -0,0 +1,16 @@
+import pytest
+
+from lesson3.tape_equilibrium import solution
+
+
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ([3, 1, 2, 4, 3], 1),
+        ([-1000, 1000], 2000),
+    ],
+)
+@pytest.mark.asyncio
+async def test_solution(numbers: list[int], expected: int):
+    """Test tape equilibrium solution"""
+    assert await solution(numbers) == expected
