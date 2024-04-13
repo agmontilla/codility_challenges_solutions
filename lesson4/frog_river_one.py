ccfcf341d31213560c78de81f49025512ca81a0c
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-04-13 13:30:20 -0300

    test: add check to frog river one challenge

--- lesson4/frog_river_one.py
+++ lesson4/frog_river_one.py
@@ -1,27 +1,12 @@
-from typing import List
+async def solution(position_x: int, falling_leaves: list[int]) -> int:
+    """Returns the earliest time when the frog can jump to the other side of the river."""
+    leaves = {}
 
+    if sum(set(falling_leaves)) - (position_x * (position_x + 1) // 2) != 0:
+        return -1
 
-class FrogRiverOne:
-    def solution(self, X: int, A: List[int]) -> int:
+    for second, position in enumerate(falling_leaves):
+        if position not in leaves:
+            leaves[position] = second
 
-        leaves = {}
-
-        if sum(set(A)) - (X * (X + 1) // 2) != 0:
-            return -1
-
-        for second, position in enumerate(A):
-            if position not in leaves:
-                leaves[position] = second
-
-        return max(leaves.values())
-
-
-if __name__ == "__main__":
-    # data_input = [1, 1, 2, 3, 5, 6, 4]
-    # x = 6
-
-    # data_input = [3, 1, 4, 2, 3, 5, 4]
-    data_input = [1, 3, 1, 4, 2, 3, 5, 4]
-    # data_input = [1, 2, 4, 5]
-    x = 5
-    print(FrogRiverOne().solution(x, data_input))
+    return max(leaves.values())
---
+++ tests/lesson4/test_frog_river_one.py
@@ -0,0 +1,17 @@
+import pytest
+
+from lesson4.frog_river_one import solution
+
+
+@pytest.mark.parametrize(
+    "x, data_input, expected",
+    [
+        (6, [1, 1, 2, 3, 5, 6, 4], 6),
+        (5, [1, 3, 1, 4, 2, 3, 5, 4], 6),
+        (4, [1, 2, 4, 5], -1),
+    ],
+)
+@pytest.mark.asyncio
+async def test_case_frog_river_one_is_working(data_input: list[int], x: int, expected: int):
+    """Should return the earliest time when the frog can jump to the other side of the river."""
+    assert await solution(x, data_input) == expected
